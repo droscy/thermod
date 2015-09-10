6d6ac4327b0afeeb99d7651cf263d7b4db50fb6c
Simone Rossetto <simros85@gmail.com>
2015-09-10 17:23:17 +0200

    Revised config file and first draft of timetable module

--- termod.py
+++ termod.py
@@ -2,30 +2,50 @@
 
 import argparse
 import logging
-import platform
+import os
 import sys
 import time
 
+from configparser import ConfigParser
+from threading import Thread, Lock, Event
+
 # TODO il package time è stato importato per gli sleep, va tolto quando si tolgono gli sleep
 # TODO thread che apre il socket
-# TODO thread che monitora la temperatura
 # TODO doppio fork per il demone
 # TODO gestione dei segnali per stoppare il demone
 
 # TODO timetable in json
-#  - status: <auto|t0|tmin|tmax|off>
+#  - status: <auto|t0|tmin|tmax|off|on>
 #  - t0, tmin, tmax: la temperatura in float
 
-prog_version = '0.0.0~alpha1'
+# TODO mettere un SMTPHandler per i log di tipo WARNING e CRITICAL
 
+prog_version = '0.0.0~alpha1'
+script_path = os.path.dirname(os.path.realpath(__file__))
+config_files = [os.path.abspath(os.path.join(script_path,'termod.conf')),
+                os.path.expanduser('~/.termod.conf'),
+                '/etc/termod/termod.conf']
+
+# class TemperatureCheckingThread(Thread):
+#     '''This thread checks the temperature and switch on/off the heating according to timetable'''
+#     
+#     def __init__(self):
+#         self.check = Event()
+#         Thread.__init__(self)
+#     
+#     def run(self):
+#         while True:
+#             pass
+
+# parsing input arguments
 parser = argparse.ArgumentParser(description='termod daemon')
 parser.add_argument('--version', action='version', version='%(prog)s {}'.format(prog_version))
 parser.add_argument('-D','--debug', action='store_true', help='start the daemon in debug mode')
 parser.add_argument('-F','--foreground', action='store_true', help='start the daemon in foreground')
-parser.add_argument('-L','--log', action='store', default=None, help='write message to log file')
-
+parser.add_argument('-L','--log', action='store', default=None, help='write messages to log file')
 args = parser.parse_args()
 
+# starting base logging system
 logger = logging.getLogger('termod')
 logger.setLevel(logging.INFO)
 
@@ -46,12 +66,28 @@ if args.log:
     logfile.setFormatter(logging.Formatter(fmt='%(asctime)s - %(name)s - %(levelname)-8s %(message)s',datefmt='%Y-%m-%d %H:%M:%S'))
     logger.addHandler(logfile)
 
-logger.debug('Starting Daemon...')
+cfg = ConfigParser()
+cfg.read_string('[global] enabled = 0')
 
-logger.info('Daemon started')
+# TODO serve una classe o un metodo o unmoduelo con il timetable per sapere se accendere
+# o spegnere penso che sia più corretto spostare tutta la logica sulla classe, così dai
+# vari thread ci si può interfacciare direttamente all'oggetto istanziato
 
-logger.info('Starting first cycle')
-for i in range(10):
-    logger.debug('counter = {}'.format(i))
-    time.sleep(1)
-logger.info('Daemon stopped')
+# main
+if __name__ == "__main__":
+    logger.debug('Reading config files...')
+    cfg.read(config_files) # TODO in caso di più file quale ha precedenza?
+
+    logger.debug('Starting socket thread...')
+    # TODO thread da scrivere
+    logger.debug('Socket thread started')
+
+    
+
+    logger.info('Daemon started')
+
+    logger.info('Starting first cycle')
+    for i in range(10):
+        logger.debug('counter = {}'.format(i))
+        time.sleep(1)
+    logger.info('Daemon stopped')
