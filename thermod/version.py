2eabc3fe82369194958b423eb6e951a425a7dde6
Simone Rossetto <simros85@gmail.com>
2016-10-08 16:11:38 +0200

    Add right requirements for thermod-web package

--- thermod/version.py
+++ thermod/version.py
@@ -20,9 +20,9 @@ along with Thermod.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 __date__ = '2016-03-27'
-__updated__ = '2016-10-02'
+__updated__ = '2016-10-08'
 
-__version__ = '1.0.0b6'
+__version__ = '1.0.0b7'
 """The main version of the whole Thermod daemon."""
 
 # vim: fileencoding=utf-8 tabstop=4 shiftwidth=4 expandtab
