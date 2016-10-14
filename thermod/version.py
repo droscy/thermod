3c091d6fece4ab4918232a6533b4b49ccd7f2667
Simone Rossetto <simros85@gmail.com>
2016-10-14 09:42:51 +0200

    Mark timetable.json as Debian conffile

--- thermod/version.py
+++ thermod/version.py
@@ -20,9 +20,9 @@ along with Thermod.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 __date__ = '2016-03-27'
-__updated__ = '2016-10-08'
+__updated__ = '2016-10-14'
 
-__version__ = '1.0.0b7'
+__version__ = '1.0.0b8'
 """The main version of the whole Thermod daemon."""
 
 # vim: fileencoding=utf-8 tabstop=4 shiftwidth=4 expandtab
