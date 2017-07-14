2e1692beb310c8913d1dd738e3bfde53ecd3e562
Simone Rossetto <simros85@gmail.com>
2017-07-14 15:27:40 +0200

    Remove fixed colour for status in Munin plugin

--- thermod/version.py
+++ thermod/version.py
@@ -20,9 +20,9 @@ along with Thermod.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 __date__ = '2016-03-27'
-__updated__ = '2017-02-20'
+__updated__ = '2017-07-14'
 
-__version__ = '1.0.0b12'
+__version__ = '1.0.0b13'
 """The main version of the whole Thermod daemon."""
 
 # vim: fileencoding=utf-8 tabstop=4 shiftwidth=4 expandtab
